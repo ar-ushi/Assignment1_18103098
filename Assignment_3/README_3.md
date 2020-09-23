938f545937d07942e0b8663a7fa9cc736974bb70
Arushi Aggarwal <aggarwalarushi5@gmail.com>
2020-09-24 01:22:22 +0530

    assignment 3

---
+++ Assignment_3/README_3.md
@@ -0,0 +1,34 @@
+<h1>Assignment 3 Part 1 </h1>
+
+<p>  Contains:
+<ul>
+    <li>
+        Two CSV files : AllUrls and Text.
+    </li>
+    <p>AllUrls stores Title + URL for each page. Text stores Tag + URL for each page.
+        For simplicity purpose,only p tag and img tag was used. However,this can be expanded to h1,h2,h3,h4 tags too
+    </p>
+</ul>
+</p>
+
+
+<h1>Assignment 3 Part 3</h1>
+<p>
+    Contains:
+    <ul>
+        <li>
+            File 1 : pdfdownload.java 
+        </li>
+        <p>
+            This stores all the pdf/txt/docx/mp4 links in a csv file. For this website,only pdf links were found.
+            For simplicity purpose,100 pdf links were extracted
+        </p>
+        <li>
+            File 2 :  download.java
+        </li>
+        <p>
+            Automates downloading of each file. 4 example pdfs are attached which have been downloaded using this program. Reads csv file made previously
+            and extracts extension for each. After which,it downloads every link mentioned and stores it in the same directory.
+        </p>
+    </ul>
+</p>
--- Assignment_3/src/Scrapper/focusedcrawl.java
+++ Assignment_3/src/Scrapper/focusedcrawl.java
@@ -11,69 +11,54 @@ import java.util.*;
 
 public class focusedcrawl{
 	public static void main(String[] args) {
-		String pathUrl = "facurls.csv",pathText = "factext.csv";
+		String pathUrl = "facurls.csv";
 		//creating a file obj for file at our csv file
-		File fileUrl = new File(pathUrl),fileText = new File(pathText);
+		File fileUrl = new File(pathUrl);
 		try {
-			FileWriter outputUrl = new FileWriter(fileUrl), outputText = new FileWriter(fileText);
-			CSVWriter urlWriter = new CSVWriter(outputUrl), textWriter = new CSVWriter(outputText);
-			String[] headerUrl = {"Text","URL"}, headerText = {"Tag","Text"};
-			urlWriter.writeNext(headerUrl);
-			textWriter.writeNext(headerText);
-			
-			//Adding Data to csv 
-			ArrayList<String> visited = new ArrayList<String>(); //list stores all links that we've parsed through
-	        Set<String> links = new HashSet<>(); //check if new url is the same as one in set so will be skipped else we go into a infinite loop
-	        Set<String> faculty = new HashSet<>();
-	        int count = 0;
-	        int depth = 1;
-	        //depth first search
-	        
-	        String seedUrl = "http://pec.ac.in";
-	        visited.add(seedUrl);
-	        links.add(seedUrl);
-	        count++;
-	        for(int i = 0; i<visited.size();i++) {
-	        	try {
-	        		String curr = visited.get(i);
-	        		Document d = Jsoup.connect(curr).get();
-	        		Elements nextLink = d.select("a[href]");
-	        		for (Element e : nextLink) {
-	        			String url = e.absUrl("href"); //absURL lets us get an entire url from an attribute
-		        		String etext = e.text(); 
-		        		if (etext.length() != 0) {
-		        			
-		        			if (!links.contains(url) && url.contains("https://pec.ac.in/")){
-		        				count++;
-		        				visited.add(url);
-		        				links.add(url);
-		        				
-		        			if ((url.contains("faculty")) && !faculty.contains(url)){
-		        				System.out.println(url);
-		        				String[] textData = {url};
-		        				//urlWriter.writeNext(textData);
-		        				faculty.add(url);
+			FileWriter outputUrl = new FileWriter(fileUrl);
+			CSVWriter urlWriter = new CSVWriter(outputUrl);
+			ArrayList<String> ar = new ArrayList<String>();     
+	        String seedUrl = "http://pec.ac.in/faculty-index"; //observe that each university has an index where they store all faculty details so seed url needs to be changed only
+	        		try {
+	        		Document d = Jsoup.connect(seedUrl).get();
+	        		Element table = d.select("table").get(0);
+	        		Elements rows =  table.select("tr");
+	        		/*Elements ths =  table.select("th");
+	        		for (Element th : ths) {
+	        			String etext = th.text(); 
+		        		if (etext.length() >= 1) {
+		        			ar.add(etext);
 		        			}
-		        			else {
-		        				visited.add(url);
-		        		        links.add(url);
-		        		        
-		        		        count++;
-		        			}
-		        		}
-		        		}
 	        		}
+	        		String[] headerUrl = new String[ar.size()];
+	        		//specifically for pec.
+	        		ar.remove(0);
+	        		headerUrl = ar.toArray(headerUrl);
+	        		urlWriter.writeNext(headerUrl);*/   //column name code
 	        		
-	        	}catch (IOException e) {
+	        		for (Element row : rows) {
+	        			Elements tds = row.select("td");
+	        			for (Element td : tds) {
+	        				ArrayList<String> visited = new ArrayList<String>();
+	        				String tdt = td.text();
+	        				if (tdt.length()>=1) {
+	        					visited.add(tdt);
+	        				}
+	        				String[] textstore = new String[visited.size()];
+	        				textstore = visited.toArray(textstore);
+	        				urlWriter.writeNext(textstore);
+	        			}
+	        			
+	        			System.out.println(tds.text());
+	        			
+	        		}
+	        	
+	        			urlWriter.close();
+		        			
+	        		} catch (IOException e) {
 	        		e.printStackTrace();
 	        		
 	        	}
-	        	if (i == 100) { //capping it off at 100 for test demo 
-	        		//urlWriter.writeNext(new String[] {"Total URLS :" + count} );
-	        		break;
-	        	}
-	        	
-	        }
 	        
 		}catch (IOException e) {
 			System.out.println(e);
